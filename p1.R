9cc0862c7378def3e56e877d87fc12c22383dda4
Lucas Amado Oseira <lucas.amado@rai.usc.es>
2025-05-26 10:39:16 +0200

    alguns ejs mais

--- p1.R
+++ p1.R
@@ -23,7 +23,7 @@ summary(KorTemp)
 # para los coeficientes del modelo. Calcula los valores ajustados del modelo y calcula la
 # suma residual de cuadrados
 z <- lm(FTMax ~ LWS + LRadSol + LLH + LTMax + LRHmax + LTmin, data = KorTemp)
-summary(z)
+summary(z) #Todas las variables que observamos están en un rango para ser consideradas signficativas
 
 coef(z) #coeficientes del modelo
 
@@ -38,8 +38,6 @@ RSE
 confint(z) #intervalos de confianza a 95%
 
 
-
-
 # b
 # Calcula el valor del coeficiente de correlación de Pearson entre la variable respuesta Y
 #y el predictor X1.
@@ -48,7 +46,13 @@ cor(KorTemp$FTMax, KorTemp$LWS) #coeficiente de correlacion entre FTMAX(y) y LWS
 # c
 #  Define y calcula el coeficiente de correlación parcial entre X1 y X2, controlando por el
 # resto de las variables explicativas.
-
+r_x1 <- residuals(lm( LWS ~ LLH + LTMax + LRHmax + LTmin, data = KorTemp))
+summary(r_x1)
+r_x2 <- residuals(lm( LRadSol ~ LLH + LTMax + LRHmax + LTmin, data = KorTemp))
+summary(r_x2)
+cor(r_x1,r_x2)
+# los rangos de r_x1 son mucho más acotados que r_x2, esto indica que el modelo para LWS,
+# ajusta mucho mejor o hay menos variabilidad en LWS, los valores de
 
 # d
 # Considera un modelo reducido que contiene solo un subconjunto de las variables explicativas incluidas en el modelo completo. Formula y contrasta, mediante un test t, la
@@ -56,13 +60,18 @@ cor(KorTemp$FTMax, KorTemp$LWS) #coeficiente de correlacion entre FTMAX(y) y LWS
 # cero.
 
 z1 <- lm(FTMax ~ LWS + LRadSol + LLH + LTMax + LRHmax, data = KorTemp) # generamos unn nuevo modelo sin la variable LTmin
-
+summary(z)
+summary(z1)
+# Observamos cambios en los valores las variables pero ningún cambio de signo, podríamos decir que no apreciamos fenómeno de confusión
 
 # e
 # Compara el modelo completo con el modelo reducido mediante un test F. Expón claramente la hipótesis nula, calcula el estadístico de contraste, determina el valor crítico
 # correspondiente y extrae las conclusiones en función del resultado del test
+summary(z)$adj.r.squared #0.5019794
+summary(z1)$adj.r.squared #0.4730303
+#podemos observar que se reduce la explicatividad en entorno a en 3%
 
-# f
+#f
 # Compara el coeficiente de determinación ajustado para ambos modelos
 
 
@@ -70,7 +79,16 @@ z1 <- lm(FTMax ~ LWS + LRadSol + LLH + LTMax + LRHmax, data = KorTemp) # generam
 # (1.5 ptos) Calcula las componentes principales de las variables del último período disponible.
 # Dibuja las puntuaciones de las dos primeras componentes respecto al mes (mes). Interpreta
 # las componentes que expliquen conjuntamente más del 75 % de la variabilidad.
+datos_L <- KorTemp[, grep("^L", names(KorTemp))]
+pca <- prcomp(na.omit(datos_L), scale. =T)
+summary(pca)
+screeplot(pca)
+screeplot(pca, type="lines")
 
+summary(pca) # a partir de la PC4 suman un acumulado de más de 75%, 78.72% concretamente
+
+pca$rotation[,1:4]
+# los valores en el primer componente son positivos para LTMax,LTMin,LTMax_D,LTmin_D
 # 3
 # (1 pto) Calcula un módelo de regresión para explicar (A1=0:FTMax, A1=1:FTmin) con las variables del último período disponible (empiezan por L), con el objetivo de usar el mejor modelo
 # con el menor número de covariables. Explica el proceso seguido, las elecciones tomadas y
