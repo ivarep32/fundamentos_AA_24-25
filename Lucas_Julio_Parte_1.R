e5d8347a5db6a6e0eceed04cecf729bf4b0757b6
Lucas Amado Oseira <lucas.amado@rai.usc.es>
2025-07-08 09:22:57 +0200

    enunciados

---
+++ Lucas_Julio_Parte_1.R
@@ -0,0 +1,44 @@
+#1.
+# Se desea predecir las emisiones de emisiones CO2 per cápita (lCO2), en función del resto
+# de variables. Ajusta por el metodo de mínimos cuadrados el siguiente modelo de regresión
+# múltiple: Y = β0 +β1X1 +β2X2 +β3X3 +β4X4 +β5X5 +β6X6 +ε donde: X1 = inflation;
+# X2 = lGDPc; X3 = GDP.growth; X4 = internet y X5 = lagv
+
+# a) Obtén los coeficientes estimados del modelo, así como intervalos de confianza al 95 %
+# para los coeficientes del modelo. Calcula los valores ajustados del modelo y calcula la
+# suma residual de cuadrados.
+
+
+
+# b) Calcula el valor del coeficiente de correlación de Pearson entre la variable respuesta Y
+# y el predictor X1.
+
+
+
+# c) Define y calcula el coeficiente de correlación parcial entre X1 y X2, controlando por el
+# resto de las variables explicativas.
+
+
+
+# d) Considera un modelo reducido que contiene solo un subconjunto de las variables explicativas
+# incluidas en el modelo completo. Formula y contrasta, mediante un test t, la
+# hipótesis nula de que los coeficientes asociados a las variables excluidas son iguales a
+# cero.
+
+
+
+# e) Compara el modelo completo con el modelo reducido mediante un test F.
+# Expón claramente la hipótesis nula, calcula el estadístico de contraste, determina el valor crítico
+# correspondiente y extrae las conclusiones en función del resultado del test.
+
+
+
+# f) Compara el coeficiente de determinación ajustado para ambos modelos.
+# Compara todos los resultados que obtienes con sus versiones manuales, verifica que coinciden e interpeta los resultados (2.5 ptos).
+
+
+
+# 2. Calcula las componentes principales de las variables numéricas (excepto country y Region)
+# e interpreta aquellas que conjuntamente expliquen al menos un 90 % de la variabilidad total.
+# Dibuja las dos primeras componentes principales en función del continente (Region) (1.5
+# ptos).
---
+++ Lucas_Julio_Parte_2.R
@@ -0,0 +1,18 @@
+# 3. Estima un modelo de regresión no paramétrico para explicar lCO2 con la variable internet
+# seleccionando el parámetro ventana de forma óptima. Dibuja el modelo de regresión obtenido. A la vista de la gráfica, ¿podemos suponer que existe una relación lineal entre ambas
+# variables? (1 pto).
+
+
+
+# 4. Estimar un modelo lineal para explicar lCO2 en función del resto de variables numéricas con
+# el objetivo de usar el menor número de covariables. (1 pto).
+
+
+
+# 5. Elaborar dos modelos de clasificación (uno basado en Bayes y otro en métodos de regresión)
+# para clasificar el continente (Region) en función de las variables numéricas. Para estos modelos
+# excluye los países de América (Region==AM) y Oceanía (Region==OC). Comenta
+# sobre los países europeos que se clasificarían mal y aplica el modelo de clasificación para
+# los países que NO han entrado en el modelo (América y Oceanía) (1 pto).
+
+
